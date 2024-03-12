959247c54d9708b96273df27592809495f0a2e1e
lorimv <lorenzomvega@gmail.com>
2024-03-12 13:01:09 -0500

    more c# learning + list parsing

--- ZipCounter/Program.cs
+++ ZipCounter/Program.cs
@@ -1,10 +1,33 @@
 ﻿namespace ZipCounter
 {
+    using System;
+    using System.IO;
+
     static class Program
     {
+        // find out why this throws a null type warning
+        static void ParseFile(String filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                using (StreamReader SR = new StreamReader(filePath))
+                {
+                    String S = SR.ReadLine();
+                    while (S != null)
+                    {
+                        Console.WriteLine(S);
+                        S = SR.ReadLine();
+                    }
+                }
+            }
+            else { Console.WriteLine("UH OH!"); }
+        }
+
         static void Main()
         {
             Console.WriteLine("main;");
+            Console.WriteLine("hi");
+            ParseFile("/home/XXXXXXXXXX/Downloads/list");
         }
     }
 }
